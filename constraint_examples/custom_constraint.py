4cf689773c834d6d1e7c6d12cce6e400cad961cb
Christopher Inegbedion <christopher.chris.ing@gmail.com>
2021-01-25 11:20:51 +0000

    Option to provide custom flag to constraint

--- constraint_examples/custom_constraint.py
+++ constraint_examples/custom_constraint.py
@@ -3,12 +3,20 @@ from flag import Flag
 
 
 class CustomConstraint(Constraint):
-    """A custom constraint class to create constraints and can be fitted with any model of choice"""
+    """A custom constraint class to create constraints that can be fitted with any model of choice.
+
+    This class enables to creation of constraints by just assigning it a name(for debugging) and
+     a model of choice to get started"""
 
     def __init__(self, name, model, flag=None):
         self.name = name
         self.model = model
-        self.flag = Flag("ConstraintVerification", False, 0, 0, False, False, "", "")
+
+        # support for providing custom flags
+        if flag is not None:
+            self.flag = flag
+        else:
+            self.flag = Flag("ConstraintVerification", False, 0, 0, False, False, "", "")
 
         super().__init__(self.name, self.flag, self.model)
         self.model.set_constraint(self)
