4d1b051edecef0bd5446023361b699988393f3c1
christopher-chris <christopher.chris.ing@gmail.com>
2021-02-27 20:14:15 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='Constraints/Stages',
+    url='https://github.com/christopher-inegbedion/ConstraintsStagesPy',
+    author='Christopher E. Inegbedion',
+    author_email='eromoseleinegbe@gmail.com',
+    # Needed to actually package something
+    packages=['constraints', 'stage'],
+    # Needed for dependencies
+    install_requires=['arrow'],
+    # *strongly* suggested for sharing
+    version='0.1-a',
+    # description='An example of a python package from pre-existing code',
+    # We will also need a readme eventually (there will be a warning)
+    long_description=open('README.md').read(),
+)
